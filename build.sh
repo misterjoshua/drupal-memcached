2c1e9b7cf10809457dabf314a24f70b913987365
Josh Kellendonk <josh@colloco.com>
2019-11-27 12:00:25 -0700

    Try using the get_version ::set-output approach to get the tag name

--- build.sh
+++ build.sh
@@ -5,6 +5,9 @@ function die() { log "$*"; exit 1; }
 
 HERE=${HERE:-$(dirname $0)}
 BUILD_DIR=${BUILD_DIR:-$HERE/build}
+VERSION=${VERSION:-$(git describe --tags || echo 0.0.0)}
+
+log "Chart version $VERSION"
 
 log "Initializing helm on the client-side"
 helm init --client-only >/dev/null
@@ -13,9 +16,6 @@ if [ ! -d "$BUILD_DIR" ]; then
     log "Creating build dir at $BUILD_DIR"
     mkdir "$BUILD_DIR"
 fi
-
-VERSION=$(git describe --tags || echo 0.0.0)
-log "Chart version $VERSION"
 
 log "Creating helm package"
 helm package . --version "$VERSION" --destination "$BUILD_DIR"
