06277e7bf63c7e75ef43cf895e65d54d07173243
Josh Kellendonk <josh@colloco.com>
2019-11-27 11:05:54 -0700

    Add logging to build script, add build dir to gitignore

--- build.sh
+++ build.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 
-mkdir build
-helm package . -d build
+function log() { echo "$*" >&2; }
+function die() { log "$*"; exit 1; }
+
+HERE=${HERE:-$(dirname $0)}
+BUILD_DIR=${BUILD_DIR:-$HERE/build}
+
+log "Initializing helm on the client-side"
+helm init --client-only >/dev/null
+
+if [ ! -d "$BUILD_DIR" ]; then
+    log "Creating build dir at $BUILD_DIR"
+    mkdir $BUILD_DIR
+fi
+
+log "Creating helm package"
+helm package . -d $BUILD_DIR
